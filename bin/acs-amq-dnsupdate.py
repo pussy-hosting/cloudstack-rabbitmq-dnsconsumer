afb9337fec5e15c5c9f64f3189809213e855c06b
Seitz, Stephan (BNotK) <s.seitz@bnotk.de>
2021-04-03 10:27:33 +0200

    changed fixed parameters to environment

--- bin/acs-amq-dnsupdate.py
+++ bin/acs-amq-dnsupdate.py
@@ -7,22 +7,28 @@ import os
 from cs import CloudStack
 
 def main():
-    amq_user     = 'cloudstack'
-    amq_pass     = 'queuediekuh'
-    amq_host     = 'localhost'
-    amq_exchange = 'cloudstack-events'
+    param = {
+            'AMQ_USERNAME': 'guest',
+            'AMQ_PASSWORD': 'guest',
+            'AMQ_HOSTNAME': 'localhost',
+            'AMQ_PORT': '5672',
+            'AMQ_EXCHANGE': 'cloudstack-events',
+            'ACS_ENDPOINT': 'http://localhost:8080/client/api',
+            'ACS_APIKEY': '',
+            'ACS_SECRETKEY': '',
+            }
 
-    cs_endpoint  = 'http://localhost:8080/client/api'
-    cs_apikey    = 'kM43Ngg64xky5z2KebBehQqhy6I2huZL0B1DBDh84L_Ii9ibWG0ib3YVO5Rzg7MEbayOghiZk0QglRbmernW2g'
-    cs_secretkey = 'RcfD7erAy-0yaSMM39A9H6PPyy958Sdc7tDAVKDmteFSw3Iw2qyJ6aoNUC80KrUAP2OXhi0twAd9sp7hXyMtDA'
+    for param_key, param_value in param.items():
+        if param_key in os.environ:
+            param[param_key] = os.getenv(param_key)
 
-    cs = CloudStack(endpoint=cs_endpoint,
-            key=cs_apikey,
-            secret=cs_secretkey)
+    cs = CloudStack(endpoint=ACS_ENDPOINT,
+            key=ACS_APIKEY,
+            secret=ACS_SECRETKEY)
 
-    credentials = pika.PlainCredentials(amq_user, amq_pass)
-    parameters = pika.ConnectionParameters(amq_host,
-            5672,
+    credentials = pika.PlainCredentials(AMQ_USERNAME, AMQ_PASSWORD)
+    parameters = pika.ConnectionParameters(AMQ_HOSTNAME,
+            int(AMQ_PORT),
             '/',
             credentials)
 
@@ -33,7 +39,7 @@ def main():
     result = channel.queue_declare(exclusive=True)
     queue_name = result.method.queue
 
-    channel.queue_bind(exchange=amq_exchange,
+    channel.queue_bind(exchange=AMQ_EXCHANGE,
             routing_key='#',
             queue=queue_name)
 
@@ -58,7 +64,7 @@ def main():
                         'status' in blist and
                         blist['instanceUuid'] == uuid and
                         blist['status'] == 'SUCCEEDED'):
-                    
+
                     # DESTROY
                     if (blist['commandEventType'] == 'VM.DESTROY'):
                         print('DESTROY VM WITH UUID: %s' % uuid)
@@ -71,7 +77,7 @@ def main():
                         vms = cs.listVirtualMachines(id=uuid, fetch_list=True)
                         if (len(vms) == 1):
                             vm = vms[0]
-                            hostname = vm['name'] 
+                            hostname = vm['name']
                             domain = ''
                             ipaddress = ''
                             ip6address = ''
@@ -88,13 +94,22 @@ def main():
                                         network = networks[0]
                                         if ('networkdomain' in network):
                                             domain = network['networkdomain']
-                            addrecords(uuid, hostname, domain, ipaddress, ip6address) 
+                            if validate_fqdn(hostname + '.' + domain):
+                                addrecords(uuid, hostname, domain, ipaddress, ip6address)
 
     channel.basic_consume(callback,
             queue=queue_name,
             no_ack=True)
 
     channel.start_consuming()
+
+def validate_fqdn(dn):
+    if dn.endswith('.'):
+        dn = dn[:-1]
+    if len(dn) < 1 or len(dn) > 253:
+        return False
+    ldh_re = re.compile('^[a-z0-9-]{2,63}$', re.IGNORECASE)
+    return all(ldh_re.match(x) for x in dn.split('.'))
 
 # Remove Nameserver Records
 def removerecords(uuid=''):
